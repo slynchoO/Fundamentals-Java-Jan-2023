3896cdb3d0c0d296ca03ccf897c9eef37cea5b8f
Gabriela Ivanova <62185813+slynchoO@users.noreply.github.com>
2023-03-07 10:07:20 +0200

    Add files via upload

---
+++ ObjAndClasses/objAndClassesExer/VehicleCatalogue/Vehicle.java
@@ -0,0 +1,48 @@
+package fundamentals.objAndClassesExer.VehicleCatalogue;
+
+public class Vehicle {
+    private String type;
+    private String model;
+    private String color;
+    private int horsePower;
+
+    public Vehicle(String type, String model,
+                   String color, int horsePower) {
+        this.type = type;
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+}
